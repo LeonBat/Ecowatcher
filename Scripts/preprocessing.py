d1c8d0ae091048fa9ed05c046d6951d27dff53ef
Leon <leon.gasteiger@web.de>
2025-11-14 07:12:27 +0100

    Converted Jupyternotebook to script

--- Scripts/preprocessing.py
+++ Scripts/preprocessing.py
@@ -1,16 +1,27 @@
-#################################
-# Preprocessing Script for data #
-#################################
+#!/usr/bin/env python
+# coding: utf-8
 
-#Libraries
+# <h1 style = "text-align:center; text-decoration: underline"> Preprocessing of environmental data </h1>
+
+# <i>Libraries</i>
+
+# In[ ]:
+
+
 import pandas as pd
 import os
 
 
+# <hr>
+# <i>Variables and Paths<i>
+
+# In[ ]:
+
+
 ###### Variables
-co2_data_path = os.path("../Data_raw/annual-co2-emissions-per-country.csv")
-energy_data_path = os.path("../Data_raw/per-capita-energy-use.csv")
-air_data_path = os.path("../Data_raw/long-run-air-pollution.csv")
+co2_data_path = os.path.join("../Data_raw/annual-co2-emissions-per-country.csv")
+energy_data_path = os.path.join("../Data_raw/per-capita-energy-use.csv")
+air_data_path = os.path.join("../Data_raw/long-run-air-pollution.csv")
 
 # Reading in data
 co2_data = pd.read_csv(co2_data_path)
@@ -18,9 +29,11 @@ energy_data = pd.read_csv(energy_data_path)
 air_data = pd.read_csv(air_data_path)
 
 
+# <hr>
+# <i>Function definitions</i>
 
+# In[ ]:
 
-###### Function definitions
 
 #Function for inspecting the datafiles
 def inspect_data(input_file: pd.DataFrame) -> None:
@@ -38,23 +51,78 @@ def inspect_data(input_file: pd.DataFrame) -> None:
     Structure of co2_data dataframe
     '''
 
+    #prints dimensions of dataframe
     print(f"Shape of {input_file} dataframe")
     input_file.shape
 
+    #prints structure of dataframe
     print(f"Info of {input_file} dataframe")
     input_file.info()
 
+    #prints the first 5 rows of dataframe
     print(f"Head of {input_file} dataframe")
-    input_file.head()
+    input_file.head(n = 5)
 
 
-#Function for adding ID columns to the files --> setting up keys for database
-def adding_IDs():
-    pass
+# <hr>
+# <b>Inspection<b>
+
+# In[ ]:
 
 
+inspect_data(co2_data)
 
 
+# In[ ]:
 
 
-####### Preprocessing
+inspect_data(air_data)
+
+
+# In[ ]:
+
+
+inspect_data(energy_data)
+
+
+# I noticed that in **co2_data** and **energy_data** there are duplicate columns namely **time** and **year**.
+# 
+# I will remove on of them, to keep the dataframe simpler
+# 
+# Although in **energy_data** the decision becomes clearer because **year_column** is the same as **time** but with missing values for 2023.
+
+# In[ ]:
+
+
+co2_data.columns
+
+
+# In[ ]:
+
+
+# Dropping duplicate columns
+co2_data = co2_data.drop(columns = ["time"])
+
+energy_data = energy_data.drop(columns = ["Year"])
+# Rename time column to year
+energy_data = energy_data.rename(columns = {"time": "Year"})
+
+
+# In[ ]:
+
+
+co2_data.head(n = 5)
+
+
+# In[ ]:
+
+
+energy_data.head(n = 5)
+
+
+# In[ ]:
+
+
+get_ipython().system('jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace preprocessing.ipynb')
+
+
